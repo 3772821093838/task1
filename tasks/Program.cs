c4a9252abd1326d8ec34a2358dfa98c955883cf7
3772821093838 <egorbystrov161@gmail.com>
2024-10-25 02:24:06 +0300

    add task2

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -4,18 +4,11 @@ public class MathProblems
 {
     public static void Main(string[] args)
     {
-        // Задача 1: Расстояние между автомобилями
-        Console.WriteLine("Задача 1: Расстояние между автомобилями");
-        Console.Write("Введите скорость первого автомобиля (V1 км/ч): ");
-        double V1 = double.Parse(Console.ReadLine());
-        Console.Write("Введите скорость второго автомобиля (V2 км/ч): ");
-        double V2 = double.Parse(Console.ReadLine());
-        Console.Write("Введите начальное расстояние между автомобилями (S км): ");
-        double S = double.Parse(Console.ReadLine());
-        Console.Write("Введите время (T часов): ");
-        double T = double.Parse(Console.ReadLine());
+        Console.WriteLine("\nЗадача 2: Преобразование температуры");
+        Console.Write("Введите температуру по Фаренгейту (TF): ");
+        double TF = double.Parse(Console.ReadLine());
 
-        double distance = S + (V1 + V2) * T;
-        Console.WriteLine($"Расстояние между автомобилями через {T} часов: {distance} км");
+        double TC = (TF - 32) * 5 / 9;
+        Console.WriteLine($"Температура по Цельсию: {TC} °C");
     }
 }
